23a8935c12c7d006c1b86294f154f63a71cbe962
Franco Cragnolini <cragnolinifranco123@gmail.com>
2023-03-17 17:28:11 -0300

    inicio del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:preferences_app/screens/screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        SettingsScreen.routeName: (context) => const SettingsScreen(),
+      },
+    );
+  }
+}
