b969dca91bfa1a95c5b01de84e1b4e1ff9b457f4
Franco Cragnolini <cragnolinifranco123@gmail.com>
2023-03-17 18:29:06 -0300

    diseño de pantallas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   static const routeName = "home_screen";
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      //? drawer puede ser cualquier  Widget peo existe un built-in Widget llamado Drawer()
+      drawer: const SideMenu(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text('isDarkmode'),
+          Divider(),
+          Text('Género'),
+          Divider(),
+          Text('Nombre de usuario'),
+          Divider(),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/widgets.dart';
 
 class SettingsScreen extends StatelessWidget {
   static const routeName = "settings_screen";
@@ -7,6 +8,43 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      drawer: const SideMenu(),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          SwitchListTile.adaptive(
+              value: true,
+              title: const Text('Darkmode'),
+              onChanged: (value) {}),
+          const Divider(),
+          RadioListTile(
+            value: 1,
+            groupValue: 1,
+            title: const Text("Masculino"),
+            onChanged: (value) {},
+          ),
+          const Divider(),
+          RadioListTile(
+            value: 2,
+            groupValue: 1,
+            title: const Text("Femenino"),
+            onChanged: (value) {},
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextFormField(
+              initialValue: "Franco",
+              decoration: const InputDecoration(
+                  labelText: "Nombre", helperText: "Nombre del Usuario"),
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
